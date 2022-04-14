977449ba93b31271b145746db54db874ffc46349
unknown <dinovasala3@gmail.com>
2022-04-14 16:38:25 +0700

    Final Commit

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -117,74 +117,219 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
-var bundleURL = null;
+})({"index.js":[function(require,module,exports) {
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
 
-function getBundleURLCached() {
-  if (!bundleURL) {
-    bundleURL = getBundleURL();
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+window.onload = function () {
+  window.slideOne();
+  window.slideTwo();
+};
+
+var sliderOne = document.getElementById("slider-1");
+var sliderTwo = document.getElementById("slider-2");
+var sliderTrack = document.querySelector(".slider-track");
+var sliderMaxValue = document.getElementById("slider-1").max;
+
+window.slideOne = function slideOne(e) {
+  var values = document.querySelector("#label-1");
+  setBubble(sliderOne, values);
+  fillColor();
+};
+
+window.slideTwo = function slideTwo() {
+  var values = document.querySelector("#label-2");
+  setBubble(sliderTwo, values);
+  fillColor();
+};
+
+window.setBubble = function setBubble(range, bubble) {
+  var val = range.value;
+  var min = range.min ? range.min : 0;
+  var max = range.max ? range.max : 100;
+  var newVal = Number((val - min) * 100 / (max - min));
+  bubble.innerHTML = "".concat(val * 20, "$");
+  bubble.style.left = "calc(".concat(newVal, "% + (").concat(10 - newVal * 0.2, "px))");
+};
+
+var buttonRiple = function buttonRiple(event) {
+  var color = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "#fff6";
+  ripple(event.currentTarget, event, 20, color);
+};
+
+window.buttonRiple = buttonRiple;
+
+window.fillColor = function fillColor() {
+  percent1 = sliderOne.value / sliderMaxValue * 100;
+  percent2 = sliderTwo.value / sliderMaxValue * 100;
+
+  if (percent2 < percent1) {
+    var temp = percent1;
+    percent1 = percent2;
+    percent2 = temp;
   }
 
-  return bundleURL;
-}
+  sliderTrack.style.background = "linear-gradient(to right, #dadae5 ".concat(percent1, "% , #3264fe ").concat(percent1, "% , #3264fe ").concat(percent2, "%, #dadae5 ").concat(percent2, "%)");
+};
 
-function getBundleURL() {
-  // Attempt to find the URL of the current script and use that as the base URL
-  try {
-    throw new Error();
-  } catch (err) {
-    var matches = ('' + err.stack).match(/(https?|file|ftp|chrome-extension|moz-extension):\/\/[^)\n]+/g);
+window.ripple = function ripple(parent, event, maxScale, color) {
+  parent.style.position = "relative";
+  parent.style.overflow = "hidden";
+  var rect = parent.getBoundingClientRect();
+  var ripple = document.createElement("span");
+  ripple.classList.add("ripple");
+  ripple.style.left = "".concat(event.clientX - rect.x, "px");
+  ripple.style.top = "".concat(event.clientY - rect.y, "px");
+  ripple.style.setProperty("--size", maxScale);
+  ripple.style.setProperty("--color", color);
+  var hasripple = parent.getElementsByClassName("ripple")[0];
 
-    if (matches) {
-      return getBaseURL(matches[0]);
-    }
+  if (hasripple) {
+    hasripple.remove();
   }
 
-  return '/';
-}
+  parent.appendChild(ripple);
+  setTimeout(function () {
+    ripple.remove();
+  }, 1200);
+};
 
-function getBaseURL(url) {
-  return ('' + url).replace(/^((?:https?|file|ftp|chrome-extension|moz-extension):\/\/.+)?\/[^/]+(?:\?.*)?$/, '$1') + '/';
-}
+var accheads = document.querySelectorAll(".accordion-head");
 
-exports.getBundleURL = getBundleURLCached;
-exports.getBaseURL = getBaseURL;
-},{}],"../node_modules/parcel-bundler/src/builtins/css-loader.js":[function(require,module,exports) {
-var bundle = require('./bundle-url');
+var _iterator = _createForOfIteratorHelper(accheads),
+    _step;
 
-function updateLink(link) {
-  var newLink = link.cloneNode();
-
-  newLink.onload = function () {
-    link.remove();
+try {
+  var _loop = function _loop() {
+    var acchead = _step.value;
+    acchead.addEventListener("click", function (event) {
+      acchead.classList.toggle("active");
+      acchead.nextSibling.nextSibling.classList.toggle("active");
+      ripple(event.currentTarget, event, 100, "#ccc");
+    });
   };
 
-  newLink.href = link.href.split('?')[0] + '?' + Date.now();
-  link.parentNode.insertBefore(newLink, link.nextSibling);
-}
-
-var cssTimeout = null;
-
-function reloadCSS() {
-  if (cssTimeout) {
-    return;
+  for (_iterator.s(); !(_step = _iterator.n()).done;) {
+    _loop();
   }
-
-  cssTimeout = setTimeout(function () {
-    var links = document.querySelectorAll('link[rel="stylesheet"]');
-
-    for (var i = 0; i < links.length; i++) {
-      if (bundle.getBaseURL(links[i].href) === bundle.getBundleURL()) {
-        updateLink(links[i]);
-      }
-    }
-
-    cssTimeout = null;
-  }, 50);
+} catch (err) {
+  _iterator.e(err);
+} finally {
+  _iterator.f();
 }
 
-module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var grids = document.querySelectorAll(".grid-item");
+
+var _iterator2 = _createForOfIteratorHelper(grids),
+    _step2;
+
+try {
+  for (_iterator2.s(); !(_step2 = _iterator2.n()).done;) {
+    var grid = _step2.value;
+    grid.addEventListener("click", function (event) {
+      ripple(event.currentTarget, event, 20, "#fff");
+
+      var _iterator4 = _createForOfIteratorHelper(grids),
+          _step4;
+
+      try {
+        for (_iterator4.s(); !(_step4 = _iterator4.n()).done;) {
+          var _grid = _step4.value;
+
+          _grid.classList.remove("active");
+        }
+      } catch (err) {
+        _iterator4.e(err);
+      } finally {
+        _iterator4.f();
+      }
+
+      event.currentTarget.classList.add("active");
+    });
+  }
+} catch (err) {
+  _iterator2.e(err);
+} finally {
+  _iterator2.f();
+}
+
+var menuLists = document.querySelectorAll(".menu-list");
+
+var toggle = function toggle(qs) {
+  document.querySelector(qs).classList.toggle("active");
+};
+
+window.toggle = toggle;
+
+var _iterator3 = _createForOfIteratorHelper(menuLists),
+    _step3;
+
+try {
+  var _loop2 = function _loop2() {
+    var menuList = _step3.value;
+    menuList.addEventListener("click", function (event) {
+      var _iterator5 = _createForOfIteratorHelper(menuList.children),
+          _step5;
+
+      try {
+        for (_iterator5.s(); !(_step5 = _iterator5.n()).done;) {
+          var child = _step5.value;
+          child.classList.remove("active");
+        }
+      } catch (err) {
+        _iterator5.e(err);
+      } finally {
+        _iterator5.f();
+      }
+
+      event.target.classList.add("active");
+
+      if (event.currentTarget.hasAttribute("has-title")) {
+        document.querySelector(event.currentTarget.getAttribute("has-title")).innerText = event.target.innerText;
+        console.log("lmao");
+      }
+    });
+  };
+
+  for (_iterator3.s(); !(_step3 = _iterator3.n()).done;) {
+    _loop2();
+  }
+} catch (err) {
+  _iterator3.e(err);
+} finally {
+  _iterator3.f();
+}
+
+var slider = document.querySelector(".carousel-images");
+var isDown = false;
+var startX;
+var scrollLeft;
+slider.addEventListener("mousedown", function (e) {
+  isDown = true;
+  slider.style.cursor = "grabbing";
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", function () {
+  isDown = false;
+  slider.style.cursor = "pointer";
+});
+slider.addEventListener("mouseup", function () {
+  isDown = false;
+  slider.style.cursor = "pointer";
+});
+slider.addEventListener("mousemove", function (e) {
+  if (!isDown) return;
+  e.preventDefault();
+  prevScroll = slider.scrollLeft;
+  var x = e.pageX - slider.offsetLeft;
+  var walk = (x - startX) * 1;
+  slider.scrollLeft = scrollLeft - walk;
+});
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -388,5 +533,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], "module")
-//# sourceMappingURL=/index.js.map
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], "module")
+//# sourceMappingURL=/src.e31bb0bc.js.map
---
+++ src/contoh.js
@@ -0,0 +1,23 @@
+const sepatu = [{
+    name: "Balenciaga", 
+    subtitle: "High speed sneakers",
+    type: "Sneakers", 
+    brand: 'Nike', 
+    borderColor: 'orange', 
+    price: '799', 
+    otherImages:['static/sepeatu.png', 'static/sepeatu.png'], 
+    detailID: '7889'
+},
+{
+
+}];
+
+const detail = [{
+    id: '7889', 
+    images: ['static/sepatu.png', 'static/sepatu.png', 'static/sepatu.png', 'static/sepatu.png'], 
+    sizes: [35, 36, 37, 40], 
+},
+{
+    
+}]
+
