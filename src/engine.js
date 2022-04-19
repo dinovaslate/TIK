dcae2cb28a4242b342b9312e70a8620b4802a4df
Haekal Dinova <dinovasala3@gmail.com>
2022-04-19 16:36:17 +0700

    Finally

---
+++ src/engine.js
@@ -0,0 +1,53 @@
+const API = [
+  {
+    brand: "Nike",
+    name: "Air Max 97",
+    type: "sneakers",
+    images: [
+      "static/sepatu2.png",
+      "static/sepatu3.png",
+      "static/sepatu4.png",
+      "static/sepatu5.png",
+    ],
+    price: 200,
+    desc: "Since its debut nearly 25 years ago, the legendary Air Max 97 has been remixed by just about everyone who's anyone in the sneaker world, from Sean Wotherspoon to Lil Nas X. Inspired by the sleek aesthetics of Japanese bullet trains (or so the story goes), its wavy lines and formidable silhouette will sync up seamlessly with the type of big, brash pants we've been championing.",
+    sizes: [36, 37, 38, 40, 37],
+    sales: 1000,
+  },
+  {
+    brand: "Nike",
+    name: "Air Foamposite 1",
+    type: "sneakers",
+    images: ["static/sepatu6.png", "static/sepatu7.png", "static/sepatu8.png"],
+    price: 350,
+    desc: "Thanks, in part, to a blockbuster collaboration with Comme des Garçons, the pendulum of popular opinion is once again swinging firmly towards Foams. We're not going to say we told you so but...we told you so.",
+    sizes: [41, 42, 43, 44, 35],
+    sales: 900,
+  },
+  {
+    brand: "Adidas",
+    name: "Adilette Shower",
+    type: "Flip flops",
+    images: ["static/sendal1.png", "static/sendal2.png", "static/sendal3.png"],
+    price: 3,
+    desc: "Put these sandals on and you're ready to go. These slides combine 3-Stripes style with comfortable cloudfoam unitsole, combined midsole and outsole for superior cushioning. It features a striking adidas logo on the side.",
+    sizes: [41, 34, 43, 44, 23],
+    sales: 800,
+  },
+  {
+    brand: "Rebook",
+    name: "Run & Sports",
+    type: "Lace up Shoes",
+    images: [
+      "static/sepatu10.png",
+      "static/sepatu11.png",
+      "static/sepatu12.png",
+    ],
+    price: 90,
+    desc: "Put these sandals on and you're ready to go. These slides combine 3-Stripes style with comfortable cloudfoam unitsole, combined midsole and outsole for superior cushioning. It features a striking adidas logo on the side.",
+    sizes: [41, 42, 36, 44, 34],
+    sales: 700,
+  },
+];
+
+export default API;
